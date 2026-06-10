07ee4a48cfdd2d549fc6f09f0092b6a04363ef20
Irsath Kareem <mohdirsath1997@gmail.com>
2026-06-10 22:15:56 +0530

    Backend setup added

---
+++ codehorn-backend/settings.gradle.kts
@@ -0,0 +1,14 @@
+rootProject.name = "Codehorn"
+
+include("gateway-service")
+include("auth-service")
+include("problems-service")
+include("problem-submission-service")
+include("contest-service")
+include("daily-challenge-service")
+include("code-submission-service")
+include("code-execution-service")
+include("cpp-execution-service")
+include("java-execution-service")
+include("python-execution-service")
+include("javascript-execution-service")
