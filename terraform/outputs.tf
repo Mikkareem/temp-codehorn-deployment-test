9eda24ad9887a7eac8de5ee8b9d29670ccc54c92
Irsath Kareem <56153409+Mikkareem@users.noreply.github.com>
2026-06-10 06:39:23 +0530

    Set argocd_initial_admin_password as sensitive
    Mark initial admin password output as sensitive to enhance security.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -21,5 +21,5 @@ output "argocd_https_nodeport" {
 output "argocd_initial_admin_password" {
   value       = data.kubernetes_secret.argocd_initial_admin_secret.data["password"]
   description = "Decoded initial admin password for Argo CD UI"
-  sensitive   = false
+  sensitive   = true
 }
