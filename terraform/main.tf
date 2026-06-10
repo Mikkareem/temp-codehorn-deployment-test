f259bb0eed6970f6717d4340550cf91f36ed1551
Irsath Kareem <mohdirsath1997@gmail.com>
2026-06-10 13:22:42 +0530

    Architecture Change 2

--- terraform/main.tf
+++ terraform/main.tf
@@ -76,6 +76,35 @@ resource "google_container_node_pool" "primary_nodes" {
   }
 }
 
+# Create Secondary Node Pool (1 Node, e2-medium)
+resource "google_container_node_pool" "secondary_nodes" {
+  name       = "${var.cluster_name}-secondary-node-pool"
+  location   = var.zone
+  cluster    = google_container_cluster.primary.name
+  node_count = 1
+
+  node_config {
+    preemptible  = false
+    machine_type = "e2-medium"
+    image_type   = "UBUNTU_CONTAINERD"
+    disk_type    = "pd-standard"
+    disk_size_gb = 40
+
+    labels = {
+      role = "general"
+    }
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/service.management.readonly",
+      "https://www.googleapis.com/auth/trace.append"
+    ]
+  }
+}
+
 # Deploy ArgoCD namespace
 resource "kubernetes_namespace" "argocd" {
   depends_on = [google_container_node_pool.primary_nodes]
